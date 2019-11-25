904d2bf871c49cc5411ab23b4cf0d7ea987df8f6
JarkimZhu <zhujiaqi.net@gmail.com>
2019-11-25 18:23:16 +0800

    添加maven依赖

--- android/app/src/main/java/cn/winapk/demo/ScrollingActivity.java
+++ android/app/src/main/java/cn/winapk/demo/ScrollingActivity.java
@@ -1,13 +1,18 @@
 package cn.winapk.demo;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
@@ -17,6 +22,7 @@ import com.google.android.material.snackbar.Snackbar;
 import java.util.Arrays;
 
 import cn.winapk.sdk.WinApk;
+
 
 public class ScrollingActivity extends AppCompatActivity {
 
@@ -31,9 +37,13 @@ public class ScrollingActivity extends AppCompatActivity {
         fab.setOnClickListener(view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show());
 
-        WinApk.INSTANCE.init(this, new WinApk.Options("123"), (slotId, event, data) -> {
+        WinApk.INSTANCE.init (
+                this,
+                new WinApk.Options("partnerId"),
+                (slotId, event, data) -> Log.d("IAdCallback", "slotId: " + slotId + ", event: " + event + ", data: " + data)
+        );
 
-        });
+        WinApk.INSTANCE.requestAllSdkPermissions(this, t -> Log.d("Permissions", "request result: " + (t == PackageManager.PERMISSION_GRANTED)));
 
         ArrayAdapter<String> adapter = new ArrayAdapter<>(
                 this, android.R.layout.simple_list_item_1,
@@ -59,7 +69,18 @@ public class ScrollingActivity extends AppCompatActivity {
     }
 
     private void showInterCut(AdapterView<?> parent) {
+        View videoView = LayoutInflater.from(this).inflate(R.layout.winapk_video_activity, null);
+        AlertDialog.Builder builder = new AlertDialog.Builder(this).setView(videoView);
+//                .setTitle("自定义dialog——登录").setIcon(R.mipmap.ic_launcher)
+//                .setPositiveButton("登录", new DialogInterface.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialogInterface, int i) {
+//                        Toast.makeText(ScrollingActivity.this, "账号： " + etUsername.getText().toString() + "  密码： " + etPassword.getText().toString()
+//                                , Toast.LENGTH_LONG).show();
+//                    }
+//                });
 
+        builder.create().show();
     }
 
     @Override
